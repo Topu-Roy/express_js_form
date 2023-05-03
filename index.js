a415f2395524d2e0b477e185f09e04380266357f
Topu Roy <topuroy143333@gmail.com.com>
2023-05-04 00:21:35 +0600

    Added functionality to submit the form

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const path = require("path");
 const port = 5050;
 
+//* Temporary Database array
+const usersArray = [];
+
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 //* Setting view engine EJS for rendering HTML
 app.set("view engine", "ejs");
@@ -11,6 +15,17 @@ app.set("view engine", "ejs");
 //* Setting up api endpoints
 app.get("/", (req, res) => {
   res.render("index");
+});
+app.get("/success", (req, res) => {
+  res.render("success");
+});
+app.get("/users", (req, res) => {
+  res.json(usersArray);
+});
+
+app.post("/", (req, res) => {
+  usersArray.push({ name: req.body.name, email: req.body.email });
+  res.redirect("/success");
 });
 
 app.listen(port, () => {
